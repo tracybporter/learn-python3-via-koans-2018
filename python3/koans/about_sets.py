033ae6242860cb8cd6079b5159f89953ff5c9dca
Tracy B Porter <tbporter@gmail.com>
2018-11-02 08:28:27 -0500

    tracy - sets and true/false

--- python3/koans/about_sets.py
+++ python3/koans/about_sets.py
@@ -3,53 +3,59 @@
 
 from runner.koan import *
 
+
 class AboutSets(Koan):
     def test_sets_make_keep_lists_unique(self):
         highlanders = ['MacLeod', 'Ramirez', 'MacLeod', 'Matunas', 'MacLeod', 'Malcolm', 'MacLeod']
 
-        there_can_only_be_only_one = set(highlanders)
+        setsOmitDuplicates = set(highlanders)
 
-        self.assertEqual(__, there_can_only_be_only_one)
+        self.assertEqual(4, setsOmitDuplicates.__len__())
 
     def test_empty_sets_have_different_syntax_to_populated_sets(self):
-        self.assertEqual(__, {1, 2, 3})
-        self.assertEqual(__, set())
+        self.assertEqual(set, {1, 2, 3}.__class__)  # not be be confused with the dictionary use of {}
+        self.assertEqual(set([1, 2, 3]), {1, 2, 3})
+        self.assertEqual(set([]), set())
 
     def test_dictionaries_and_sets_use_same_curly_braces(self):
         # Note: Literal sets using braces were introduced in python 3.
         #       They were also backported to python 2.7.
 
-        self.assertEqual(__, {1, 2, 3}.__class__)
-        self.assertEqual(__, {'one': 1, 'two': 2}.__class__)
+        self.assertEqual(dict, {'one': 1, 'two': 2}.__class__)
 
-        self.assertEqual(__, {}.__class__)
+        self.assertEqual(dict, {}.__class__)
 
     def test_creating_sets_using_strings(self):
-        self.assertEqual(__, {'12345'})
-        self.assertEqual(__, set('12345'))
+        self.assertEqual(set(['12345']), {'12345'})
+        self.assertEqual({'1', '2', '3', '4', '5'}, set('12345'))
 
     def test_convert_the_set_into_a_list_to_sort_it(self):
-        self.assertEqual(__, sorted(set('12345')))
+        # sorted takes an interable - it is not obvious to me how the following statement
+        # converted the set to a list as stated in the method name
+        self.assertEqual(['1', '2', '3', '4', '5'], sorted(set('54321')))
 
     # ------------------------------------------------------------------
 
     def test_set_have_arithmetic_operators(self):
+        # does this suggest that other iterables do not have similar capabilities?
         scotsmen = {'MacLeod', 'Wallace', 'Willie'}
         warriors = {'MacLeod', 'Wallace', 'Leonidas'}
 
-        self.assertEqual(__, scotsmen - warriors)
-        self.assertEqual(__, scotsmen | warriors)
-        self.assertEqual(__, scotsmen & warriors)
-        self.assertEqual(__, scotsmen ^ warriors)
+        self.assertEqual({'Willie'}, scotsmen - warriors)
+        self.assertEqual({'MacLeod', 'Wallace', 'Willie', 'Leonidas'}, scotsmen | warriors)
+        self.assertEqual({'MacLeod', 'Wallace'}, scotsmen & warriors)
+        # symmetric_difference --> new set with elements in either s or t but not both
+        # python is soooo cooooool
+        self.assertEqual({'Willie', 'Leonidas'}, scotsmen ^ warriors)
 
     # ------------------------------------------------------------------
 
     def test_we_can_query_set_membership(self):
-        self.assertEqual(__, 127 in {127, 0, 0, 1} )
-        self.assertEqual(__, 'cow' not in set('apocalypse now') )
+        self.assertEqual(True, 127 in {127, 0, 0, 1})
+        self.assertEqual(True, 'cow' not in set('apocalypse now'))
 
     def test_we_can_compare_subsets(self):
-        self.assertEqual(__, set('cake') <= set('cherry cake'))
-        self.assertEqual(__, set('cake').issubset(set('cherry cake')) )
+        self.assertEqual(True, set('cake') <= set('cherry cake'))
+        self.assertEqual(True, set('cake').issubset(set('cherry cake')))
 
-        self.assertEqual(__, set('cake') > set('pie'))
+        self.assertEqual(False, set('cake') > set('pie'))
